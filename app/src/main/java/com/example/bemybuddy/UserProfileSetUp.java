bc1f894220571d923f259a75eef7a7da4d7cde30
hamdamare <hamare@ualberta.ca>
2019-06-25 15:22:34 -0600

    created the dummy UI for user setup

---
+++ app/src/main/java/com/example/bemybuddy/UserProfileSetUp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018 Term x. CMPUT 301. university of Alberta. All Rights Reserved . You may use , distrubute, or modify the code under terms and conditions of the code of Students Behaviour at University of Alberta. You can find a copy og yhe liscense on the project. Otherwise please contact contat@abc.ca
+ */
+
+package com.example.bemybuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserProfileSetUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_set_up);
+    }
+}
